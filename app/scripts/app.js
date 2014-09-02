2dca2602d7551aba664210e496eaf0d70c63f056
GeorgeXu <xuge123@gmail.com>
2014-09-02 17:45:01 +0800

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ossClientUiApp
+ * @description
+ * # ossClientUiApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ossClientUiApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ossClientUiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ossClientUiApp
+ */
+angular.module('ossClientUiApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
